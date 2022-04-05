6c22db2f2baea45fae16473752e68eb47b0630ad
remenok200 <remenok9@gmail.com>
2022-04-05 21:26:12 +0300

    Add closure examples

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const arr = [];
+"use strict";
 
-func(prompt('Сколько раз вызываем функцию?'));
+function counter() {
+  let i = 0;
 
-function func(count) {
-  if(count > 0) {
-  arr.push(prompt(`Какую строку Вы хотите докинуть в массив? Осталось ${count - 1} раз.`));
-  func(count - 1);
+  function getCounter() {
+    return i;
   }
+
+  function increment() {
+    i++;
+    return i;
+  }
+
+  function decrement() {
+    i--;
+    return i;
+  }
+
+  return {
+    getCounter: getCounter,
+    increment: increment,
+    decrement: decrement,
+  };
 }
 
-console.log(arr);
+// TEST
+const obj = new counter();
+console.log(`Счетчик до манипуляций: ${obj.getCounter()}`);
+obj.increment();
+console.log(`Счетчик после инкремента: ${obj.getCounter()}`);
+obj.decrement();
+console.log(`Счетчик после декремента: ${obj.getCounter()}`);
