2ec6ea4c940e8ce5d923f8c22751a7d3042fd87f
Roman Ratushnyi <remenok9@gmail.com>
2022-04-08 18:51:58 +0300

    Add data-attributes example

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-"use strict";
+const div = document.querySelector('#root');
 
-function counter() {
-  let i = 0;
-
-  function getCounter() {
-    return i;
-  }
-
-  function increment() {
-    i++;
-    return i;
-  }
-
-  function decrement() {
-    i--;
-    return i;
-  }
-
-  return {
-    getCounter: getCounter,
-    increment: increment,
-    decrement: decrement,
-  };
+function listener(e) {
+  div.style.backgroundColor = e.target.dataset.color;
 }
 
-// TEST
-const obj = new counter();
-console.log(`Счетчик до манипуляций: ${obj.getCounter()}`);
-obj.increment();
-console.log(`Счетчик после инкремента: ${obj.getCounter()}`);
-obj.decrement();
-console.log(`Счетчик после декремента: ${obj.getCounter()}`);
+div.addEventListener('click', listener);
