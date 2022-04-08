08a172fa37f157b2be3092f73b24795f67f58248
Roman Ratushnyi <remenok9@gmail.com>
2022-04-08 20:46:37 +0300

    Add TodoList example

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const div = document.querySelector('#root');
+const todoState = [];
+
+const form = document.querySelector('#addTask');
+const taskList = document.querySelector('#taskList');
 
 function listener(e) {
-  div.style.backgroundColor = e.target.dataset.color;
+  e.preventDefault();
+  
+  todoState.push(form.elements.task.value);
+  
+  const message = document.createElement('li');
+  message.textContent = form.elements.task.value;
+  taskList.append(message);
+
+  form.reset();
 }
 
-div.addEventListener('click', listener);
+form.addEventListener('submit', listener);
